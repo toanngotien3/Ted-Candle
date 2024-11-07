98d92fa67a1955c32dfb1dfeba4f9798d0be335b
ToanNT <toannt@solashi.com>
2024-11-08 01:21:42 +0700

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css'
-import { Header } from './components/layouts/Header'
-import { Footer } from './components/layouts/Footer'
 import { Home } from './pages/Home'
 
 function App() {
